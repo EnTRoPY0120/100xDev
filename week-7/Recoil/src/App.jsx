3351befbd72c2a91893dd7888fb846253de8b798
EnTRoPY0120 <vijayarajdvr@gmail.com>
2024-02-08 21:27:32 +0530

    Recoil Learnt State management and selector in Recoil

---
+++ week-7/Recoil/src/App.jsx
@@ -0,0 +1,64 @@
+import {
+  RecoilRoot,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
+} from "recoil";
+import { countAtom, evenSelector } from "./store/atoms/count";
+
+function App() {
+  return (
+    <div>
+      <RecoilRoot>
+        <Count />
+      </RecoilRoot>
+    </div>
+  );
+}
+
+function Count() {
+  return (
+    <div>
+      <CountRenderer />
+      <Buttons />
+    </div>
+  );
+}
+
+function EvenRenderer() {
+  const isEven = useRecoilValue(evenSelector);
+  return <div>{isEven ? "its even" : null} </div>;
+}
+
+function CountRenderer() {
+  const count = useRecoilValue(countAtom);
+  return (
+    <div>
+      <b>{count}</b>
+      <EvenRenderer />
+    </div>
+  );
+}
+
+function Buttons() {
+  const setCount = useSetRecoilState(countAtom);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount((count) => count + 1);
+        }}
+      >
+        Increase
+      </button>
+      <button
+        onClick={() => {
+          setCount((count) => count - 1);
+        }}
+      >
+        Decrease
+      </button>
+    </div>
+  );
+}
+export default App;
