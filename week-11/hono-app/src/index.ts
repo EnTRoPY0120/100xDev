a6e1a9452a317ceffbb349e45b57f3417b5c4d56
EnTRoPY0120 <vijayarajdvr@gmail.com>
2024-02-22 18:54:42 +0530

    Serverless Learnt what are serverless functions and opened a cloudflare acc and got myself a domain to host my code , also learnt abt the Hono framework to help us deploy to cloudflare workers

---
+++ week-11/hono-app/src/index.ts
@@ -0,0 +1,26 @@
+import { Hono, Next } from 'hono'
+import { Context } from 'hono/jsx';
+
+const app = new Hono()
+
+async function authMiddleware(c:any, next:any) {
+  if (c.req.header("Authorization")) {
+    // Do validation
+    await next()
+  } else {
+    return c.text("You dont have access");
+  }
+}
+
+app.use(authMiddleware)
+
+app.get('/', async (c) => {
+  const body = await c.req.parseBody()
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+
+  return c.json({msg: "as"})
+})
+
+export default app
