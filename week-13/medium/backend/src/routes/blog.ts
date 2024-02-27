4533eaaba59ff3e60d18c3eaa19488a7697dbbf2
EnTRoPY0120 <vijayarajdvr@gmail.com>
2024-02-27 12:38:38 +0530

    Added type checking for all the inputs and imported my own package that was published to npm Almost done with the backend , need to touch up some places

--- week-13/medium/backend/src/routes/blog.ts
+++ week-13/medium/backend/src/routes/blog.ts
@@ -1,61 +1,70 @@
 import { Hono } from "hono";
+import { createPostInput, updatePostInput } from "@vj-npm/common-app";
 
 export const blogRouter = new Hono<{
-    Bindings:{
-        DATABASE_URL :string
-    },
-    Variables:{
-        userId :string
-        prisma : any
-    }
+  Bindings: {
+    DATABASE_URL: string;
+  };
+  Variables: {
+    userId: string;
+    prisma: any;
+  };
 }>();
 
-
 blogRouter.post("/", async (c) => {
-    const userId = c.get('userId');
+  const userId = c.get("userId");
 
-  const prisma = c.get('prisma');
+  const prisma = c.get("prisma");
   const body = await c.req.json();
-    const post = await prisma.post.create({
-        data:{
-            title : body.title,
-            content : body.content,
-            authorId : userId
-        }
-    })
-    c.status(201);
-    return c.json({ id: post.id  })
+  const { success } = createPostInput.safeParse(body);
+  if (!success) {
+    c.status(403);
+    return c.json({ message: "Invalid inputs" });
+  }
+  const post = await prisma.post.create({
+    data: {
+      title: body.title,
+      content: body.content,
+      authorId: userId,
+    },
+  });
+  c.status(201);
+  return c.json({ id: post.id });
 });
 
 blogRouter.put("/", async (c) => {
-    const userId = c.get('userId');
-  const prisma = c.get('prisma');
-      const body = await c.req.json();
-    const post = await prisma.post.update({
-        where:{
-            id : body.id,
-            authorId: userId,
-        },
-        data :{
-            title : body.title,
-            content: body.content
-        }
-    })
-    c.status(201);
-    return c.json({ message : 'Post has been updated'})
+  const userId = c.get("userId");
+  const prisma = c.get("prisma");
+  const body = await c.req.json();
+  const { success } = updatePostInput.safeParse(body);
+  if (!success) {
+    c.status(403);
+    return c.json({ message: "Invalid inputs" });
+  }
+  const post = await prisma.post.update({
+    where: {
+      id: body.id,
+      authorId: userId,
+    },
+    data: {
+      title: body.title,
+      content: body.content,
+    },
+  });
+  c.status(201);
+  return c.json({ message: "Post has been updated" });
 });
 
 blogRouter.get("/:id", async (c) => {
-  const id = c.req.param("id"); 
-    
-  const prisma = c.get('prisma');
-    const post = await prisma.post.findUnique({
-    where :{
-        id
-    }
-  })
+  const id = c.req.param("id");
+
+  const prisma = c.get("prisma");
+  const post = await prisma.post.findUnique({
+    where: {
+      id,
+    },
+  });
 
   c.status(200);
-  return c.json( post )
-
+  return c.json(post);
 });
--- week-13/medium/backend/src/routes/user.ts
+++ week-13/medium/backend/src/routes/user.ts
@@ -1,55 +1,65 @@
 import { Hono } from "hono";
-import { sign } from 'hono/jwt'
+import { sign } from "hono/jwt";
+import { signupInput, signinInput } from "@vj-npm/common-app";
 
 export const userRouter = new Hono<{
-    Bindings:{
-        DATABASE_URL: string
-        JWT_SECRET: string
-    },
-    Variables:{
-        prisma : any
-    }
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+  Variables: {
+    prisma: any;
+  };
 }>();
 
 userRouter.post("/signup", async (c) => {
-  const prisma = c.get('prisma');
+  const prisma = c.get("prisma");
   const body = await c.req.json();
-  try{
-  const user = await prisma.user.create({
-    data:{
-        email:body.email,
-        password:body.password
-    }
-  })
-
-  const token = await sign({id: user.id },c.env.JWT_SECRET);
-  return c.json({token});
-  } catch(err){
+  const { success } = signupInput.safeParse(body);
+  if (!success) {
     c.status(403);
-    return c.json({ error:"Error while signing up"})
+    return c.json({ message: "Invalid inputs" });
   }
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+      },
+    });
 
+    const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({ token });
+  } catch (err) {
+    c.status(403);
+    return c.json({ error: "Error while signing up" });
+  }
 });
 
-userRouter.post('/signin', async (c) => { 
-  const prisma = c.get('prisma');
-    const body = await c.req.json();
-	const user = await prisma.user.findUnique({
-		where: {
-			email: body.email,
-      password : body.password
-		}
-	});
-
-  if(!user){
+userRouter.post("/signin", async (c) => {
+  const prisma = c.get("prisma");
+  const body = await c.req.json();
+  const { success } = signinInput.safeParse(body);
+  if (!success) {
     c.status(403);
-    return c.json({error:" User not found"})
+    return c.json({ message: "Invalid inputs" });
   }
 
-  const token = await sign({ id: user.id}, c.env.JWT_SECRET);
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+      password: body.password,
+    },
+  });
+
+  if (!user) {
+    c.status(403);
+    return c.json({ error: " User not found" });
+  }
+
+  const token = await sign({ id: user.id }, c.env.JWT_SECRET);
   return c.json({
     message: "You have signed in successfully",
-    token})
+    token,
+  });
 });
-
-
