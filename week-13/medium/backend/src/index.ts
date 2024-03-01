4ac607a21fb1335bac47ac8985aa048304ff6f27
EnTRoPY0120 <vijayarajdvr@gmail.com>
2024-03-01 18:04:10 +0530

    Fixed typo

--- week-13/medium/backend/src/index.ts
+++ week-13/medium/backend/src/index.ts
@@ -20,11 +20,18 @@ const app = new Hono<{
 app.use("/api/v1/blog/*", async (c, next) => {
   const header = c.req.header("Authorization") || "";
   const token = header.split(" ")[1];
-  const response = await verify(token, c.env.JWT_SECRET);
-  if (response.id) {
-    c.set("userId", response.id);
-    await next();
-  } else {
+  try {
+    const response = await verify(token, c.env.JWT_SECRET);
+    if (response.id) {
+      c.set("userId", response.id);
+      await next();
+    } else {
+      c.status(401);
+      return c.json({
+        error: "Unauthorized",
+      });
+    }
+  } catch (e) {
     c.status(401);
     return c.json({
       error: "Unauthorized",
