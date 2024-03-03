28a0d05f2ea2ea4dfe5e4545fc059341d3090439
EnTRoPY0120 <vijayarajdvr@gmail.com>
2024-03-03 12:52:02 +0530

    Added Publish.tsx for publishing blogs

--- week-13/medium/frontend/src/components/RenderBlog.tsx
+++ week-13/medium/frontend/src/components/RenderBlog.tsx
@@ -1,12 +1,13 @@
 import { AppBar } from "./AppBar"
 import { Blog } from "../hooks"
+import { Avatar } from "./BlogCard"
 
 export const RenderBlog = ({blog}: {blog: Blog}) => {
     return <div>
         <AppBar/>
         <div className="flex justify-center">
-            <div className="grid grid-cols-12 px-10 w-full pt-200 max-w-screen-2xl">
-                <div className=" col-span-8">
+            <div className="grid grid-cols-12 px-10 pt-12 max-w-screen-xl">
+                <div className="col-span-8 gap-8 ">
                     <div className="text-5xl font-extrabold">
                         {blog.title}
                     </div>
@@ -17,8 +18,23 @@ export const RenderBlog = ({blog}: {blog: Blog}) => {
                         {blog.content}
                     </div>
                 </div>
-                <div className=" col-span-4">
-                    hello
+                <div className="col-span-4 ">
+                    <div className="text-slate-600 text-lg">
+                        Author
+                    </div>
+                    <div className="flex w-full">
+                        <div className="pr-4 flex flex-col justify-center">
+                            <Avatar name={blog.author.name || "Anonymous"} size="big"/>
+                        </div>
+                        <div>
+                            <div className=" text-xl font-bold">
+                                {blog.author.name || "Anonymous" }
+                            </div>
+                            <div className="pt-2 text-slate-500">
+                                A random catch phrase 
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
